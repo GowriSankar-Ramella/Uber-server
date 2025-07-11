9d28568b6368c608e1037c4bb0556289057b9726
Gowri Sankar <gowrisankarramella@gmail.com>
2025-07-11 15:29:08 +0530

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 const userRouter = require("./routes/userRoutes")
 const captainRouter = require("./routes/captainRoutes")
 
@@ -8,6 +9,12 @@ const app = express()
 app.use(express.json())
 
 app.use(cookieParser())
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
 
 app.use(express.urlencoded({ extended: true }));
 
